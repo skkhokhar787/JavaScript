b18acc32b24c2c28f02fe80882fe114bd89ea768
sk787 <skkhokhar787@gmail.com>
2026-03-27 23:43:54 +0500

    refactor: update HTML structure and improve CSS styles for message container

--- Tic-Tac_Toe/script.js
+++ Tic-Tac_Toe/script.js
@@ -1,6 +1,7 @@
 let boxes = document.querySelectorAll(".box");
 let resetBtn = document.querySelector("#reset-btn");
 let newGameBtn = document.querySelector("#new-gameBtn");
+let msgContaniar = document.querySelector(".msg-container");
 
 
 let turnO = true;
